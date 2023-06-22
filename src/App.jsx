3fab8ca595736458c068becb2c2c34d8ca8ced5e
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-06-22 13:52:07 -0600

    Add: Local Storage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Form from './components/Form';
 import Header from './components/Header';
 import PatientList from './components/PatientList';
 
 function App() {
 
-  const [ patients, setPatients ] = useState([]);
+  const [ patients, setPatients ] = useState( JSON.parse(localStorage.getItem('patients')) ?? [] );
   const [ patient, setPatient ] = useState({});
+
+  useEffect( () => {
+    localStorage.setItem('patients', JSON.stringify( patients ));
+  }, [ patients ])
 
   const deletePatient = ( id ) => {
     const updatedPatients =  patients.filter( patient => patient.id !== id );
